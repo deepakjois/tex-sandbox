a9ab42f7f76be8449fc6086d3c4093733e25f978
Deepak Jois <deepak.jois@gmail.com>
2015-11-17 12:44:48 +0530

    Compile against Lua in LuaTeX source tree

--- luatex-lua-c-bridge/compile.sh
+++ luatex-lua-c-bridge/compile.sh
@@ -1,6 +1,10 @@
 # Taken from: http://lua-users.org/wiki/BuildingModules
 # Not very robust. Works only on Mac OS X.
+CC=cc
+SVN=/Users/deepak/code/personal
+L=$SVN/trunk/source/libs/lua52/lua-5.2.4/src
 rm -rf *.o *.la *.lo .libs/
-gcc -O2 -fpic -c harfbuzz.c
-gcc -O -shared -fpic -flat_namespace -undefined suppress -undefined dynamic_lookup -o harfbuzz.so harfbuzz.o
+$CC -O2 -fpic -c harfbuzz.c
+$CC -O -fpic -dynamiclib -undefined dynamic_lookup -o harfbuzz.so harfbuzz.o
+$SVN/trunk/build/texk/web2c/luatex -luaonly test.lua
 
